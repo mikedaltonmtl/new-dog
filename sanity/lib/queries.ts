a3a0b097844d5696618106b0be9d7ae8e6f11a46
mike-dalton <michael.j.dalton@gmail.com>
2023-07-23 20:29:00 -0400

    add sanity query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,6 +1,7 @@
 import { groq } from "next-sanity";
 
 // Get all posts
-export const postsQuery = groq`*[_type == "post"]{
-    ...,
-  }`;
+// export const postsQuery = groq`*[_type == "post"]{
+//     ...,
+//   }`;
+export const postsQuery = groq`*[_type == "post"]`;
